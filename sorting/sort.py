64afb92136b3af88642c18d79217cc8a7fb69ab2
Reece Heineke <reece.heineke@gmail.com>
2017-03-10 20:52:35 -0500

    ENH: Insertion sort 3 not obviously faster (#1)

--- sorting/sort.py
+++ sorting/sort.py
@@ -4,6 +4,13 @@ from copy import copy
 def sorted_insertion_1(iterable):
     """
     Insertion Sort 1 from Programming Pearls by Jon Bentley
+
+    TODO(rheineke):
+    A custom key function can be supplied to customise the sort order, and the
+    reverse flag can be set to request the result in descending order.
+
+    :param iterable: list of items
+    :return: new list containing all items from the iterable in ascending order
     """
     result = copy(iterable)
     for i in range(len(result)):
@@ -11,5 +18,24 @@ def sorted_insertion_1(iterable):
             if result[j-1] < result[j]:
                 break
             result[j-1], result[j] = result[j], result[j-1]
+
+    return result
+
+
+def sorted_insertion_3(iterable):
+    """
+    Insertion Sort 3 from Programming Pearls by Jon Bentley
+    :param iterable: list of items
+    :return: new list containing all items from the iterable in ascending order
+    """
+    result = copy(iterable)
+    for i in range(len(result)):
+        t = result[i]
+        j = i
+        for j in reversed(range(0, i+1)):
+            if result[j-1] < t:
+                break
+            result[j] = result[j-1]
+        result[j] = t
 
     return result
--- sorting/sort_test.py
+++ sorting/sort_test.py
@@ -13,21 +13,35 @@ class SortedInsertion1Test(unittest.TestCase):
         sorted_xs = sort.sorted_insertion_1(xs)
         self.assertEqual(sorted(xs), sorted_xs)
 
-    def test_big_oh(self):
-        num = 1000
-        data = [
-            ('time', [0.] * num),
-            ('n', [0] * num)
-        ]
-        df = pd.DataFrame.from_items(data)
-        for i in range(num):
-            xs = [3, 1, 4, 2]  # Random number generation
-            start = time.perf_counter()
-            sorted_xs = sort.sorted_insertion_1(xs)
-            end = time.perf_counter()
-            df.loc[i] = [end - start, len(sorted_xs)]
-        factor_srs = df.apply(_nanosecond_factor, axis=1)
-        print('Run time: {:.1f}n^2 nanoseconds'.format(factor_srs.mean()))
+    def test_time_complexity(self):
+        time_complexity_factor(sort.sorted_insertion_1)
+
+
+class SortedInsertion3Test(unittest.TestCase):
+    def test_example(self):
+        xs = [3, 1, 4, 2]
+        sorted_xs = sort.sorted_insertion_3(xs)
+        self.assertEqual(sorted(xs), sorted_xs)
+
+    def test_time_complexity(self):
+        time_complexity_factor(sort.sorted_insertion_3)
+
+
+def time_complexity_factor(sort_function):
+    num = 1000
+    data = [
+        ('time', [0.] * num),
+        ('n', [0] * num)
+    ]
+    df = pd.DataFrame.from_items(data)
+    for i in range(num):
+        xs = [3, 1, 4, 2]  # Random number generation
+        start = time.perf_counter()
+        sorted_xs = sort_function(xs)
+        end = time.perf_counter()
+        df.loc[i] = [end - start, len(sorted_xs)]
+    factor_srs = df.apply(_nanosecond_factor, axis=1)
+    print('Run time: {:.1f}n^2 nanoseconds'.format(factor_srs.mean()))
 
 
 def _nanosecond_factor(srs):
