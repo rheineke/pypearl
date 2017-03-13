b53c62205f4c30abf07299188ddfb69106820357
Reece Heineke <reece.heineke@gmail.com>
2017-03-12 21:47:42 -0400

    ENH: QuickSort 2 (#1)

--- sorting/sort.py
+++ sorting/sort.py
@@ -48,11 +48,11 @@ def sorted_quick_1(iterable):
     :return: new list containing all items from the iterable in ascending order
     """
     result = copy(iterable)
-    _qsort(result, 0, len(result)-1)
+    _qsort_1(result, 0, len(result) - 1)
     return result
 
 
-def _qsort(iterable, lower, upper):
+def _qsort_1(iterable, lower, upper):
     if lower >= upper:
         # At most one element; do nothing
         return
@@ -66,6 +66,34 @@ def _qsort(iterable, lower, upper):
             iterable[m], iterable[i] = iterable[i], iterable[m]
     iterable[m], iterable[lower] = iterable[lower], iterable[m]
     # iterable[lower,...,m-1] < iterable[m] <= iterable[m+1,...,upper]
-    _qsort(iterable, lower, m-1)
-    _qsort(iterable, m+1, upper)
+    _qsort_1(iterable, lower, m - 1)
+    _qsort_1(iterable, m + 1, upper)
 
+
+def sorted_quick_2(iterable):
+    """
+    QuickSort 2 from Programming Pearls by Jon Bentley
+    :param iterable: list of items
+    :return: new list containing all items from the iterable in ascending order
+    """
+    result = copy(iterable)
+    _qsort_2(result, 0, len(result) - 1)
+    return result
+
+
+def _qsort_2(iterable, lower, upper):
+    if lower >= upper:
+        # At most one element; do nothing
+        return
+    m = upper + 1
+    for i in reversed(range(lower, upper+1)):
+        # invariant:
+        # TODO(rheineke): Change invariants
+        # iterable[lower+1,..., m] < iterable[lower] &&
+        # iterable[m+1,..., i-1] >= iterable[l]
+        if iterable[i] >= iterable[lower]:
+            m -= 1
+            iterable[m], iterable[i] = iterable[i], iterable[m]
+    # iterable[lower,...,m-1] < iterable[m] <= iterable[m+1,...,upper]
+    _qsort_2(iterable, lower, m - 1)
+    _qsort_2(iterable, m + 1, upper)
